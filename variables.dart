13c15efde11c134c856944421ea8178584aac601
YousufQadri <s.yousuf@live.com>
2019-10-19 15:35:14 +0500

    Basic dart syntax

---
+++ variables.dart
@@ -0,0 +1,8 @@
+// Dart's program main entry point
+void main() {
+  // Single string output
+  print('Hello World!');
+
+  // String concatenation
+  print('Hello World!' + ' this is another string');
+}
