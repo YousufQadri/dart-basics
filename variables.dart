7826dc93a08ba81a82aaef45dda2a3796dfb7d00
YousufQadri <s.yousuf@live.com>
2019-10-19 15:58:06 +0500

    Variables in Dart

--- variables.dart
+++ variables.dart
@@ -1,8 +1,30 @@
 // Dart's program main entry point
 void main() {
+  // BASICS
   // Single string output
   print('Hello World!');
 
   // String concatenation
   print('Hello World!' + ' this is another string');
+
+  // VARIABLES
+
+  // Uninitialized variables in JS are undefinned
+  // Uninitialized variables in DART are null
+
+  int age = 29; // Integer (non-decimal values)
+  String myName = "Syed Yousuf Qadri"; //String (use single or double quotes)
+  bool isTrue = true; // Boolean
+  double price = 30.55; // Integer (having decimal values)
+
+  // var - auto type checks according to the value
+  var country = 'Pakistan';
+  // Type of variable created with (var) cannot be changed
+  // ERRO: country = 123; /* Cannot change type */
+
+  // Dynamic type
+  dynamic company = "Google";
+
+  // Overwrites the current data-type value
+  company = 34;
 }
